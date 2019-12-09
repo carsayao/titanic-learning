d292e8df191d0336b1b2aa41dd9879cb801e4c98
Carlos Miguel Sayao <carsayao@pdx.edu>
2019-12-09 02:55:50 -0800

    lda, plot()

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import perceptron
 import enum
+import matplotlib.pyplot as plt
 
 # Lazy algorithms
 from sklearn.svm import SVC
@@ -11,8 +12,66 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import Perceptron
 
+def dropData(trainData, testData):
+
+    """ Data can be put back into the dataframe by commenting out the assignments
+    """
+    # this block drops ALL cabin data from the data frame
+    cols = [c for c in trainData.columns if c[:5] != 'Cabin']
+#    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:5] != 'Cabin']
+#    testData = testData[cols]
+
+    # drops ALL embarkment locations
+    cols = [c for c in trainData.columns if c[:8] != 'Embarked']
+    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:8] != 'Embarked']
+    testData = testData[cols]
+
+    # drops ALL tickets
+    cols = [c for c in trainData.columns if c[:6] != 'Ticket']
+    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:6] != 'Ticket']
+    testData = testData[cols]
+
+    # drops ALL sex
+    cols = [c for c in trainData.columns if c[:3] != 'Sex']
+#    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:3] != 'Sex']
+#    testData = testData[cols]
+    
+    # drops ALL Fare
+    cols = [c for c in trainData.columns if c[:4] != 'Fare']
+    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:4] != 'Fare']
+    testData = testData[cols]
+
+    # drop sibling/spouse
+#    cols = [c for c in trainData.columns if c[:5] != 'SibSp']
+    trainData = trainData[cols]
+#    cols = [c for c in testData.columns if c[:5] != 'SibSp']
+    testData = testData[cols]
+
+    #drop parch
+    cols = [c for c in trainData.columns if c[:5] != 'Parch']
+#    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:5] != 'Parch']
+#    testData = testData[cols]
+
+    #drop Age
+    cols = [c for c in trainData.columns if c[:3] != 'Age']
+    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:3] != 'Age']
+    testData = testData[cols]
+
+    #drop title
+    cols = [c for c in trainData.columns if c[:5] != 'Title']
+    trainData = trainData[cols]
+    cols = [c for c in testData.columns if c[:5] != 'Title']
+    testData = testData[cols]
 
 def main():
 
@@ -33,7 +92,9 @@ def main():
     model_MNB = MultinomialNB()
     model_MLP = MLPClassifier(hidden_layer_sizes=(n,n,n), activation='logistic', max_iter=2000)
     # 3 hidden layers (n, n, n)
-    model_Perceptron = Perceptron(n_iter_no_change= 10)
+    testScore = 0
+    targetLabels = train['Survived'].values
+    testLabels = test['Survived'].values
 
     model.fit(trainData, train['Survived'])
     model_GNB.fit(trainData, train['Survived'])
@@ -74,8 +135,13 @@ def main():
     testScore = model_Perceptron.score(testData, test['Survived'])*100
     print("Perceptron Learning Algorithm results:\n", "Train: ", trainScore, "%", "Test: ", testScore, "%\n\n\n")
 
+
+
     # Some PCA magic, reduce down to n parameters
-    pca = PCA(n_components=4)
+
+    numComp = 2
+
+    pca = PCA(n_components=numComp)
     pca.fit(trainData)
     trainData = pca.transform(trainData)
     testData = pca.transform(testData)
@@ -84,6 +150,7 @@ def main():
     trainScore = model_MLP.score(trainData, train['Survived'])*100
     testScore = model_MLP.score(testData, test['Survived'])*100
     print("(PCA) Multi-Layer Perceptron results:\n", "Train: ", trainScore, "%", "Test: ", testScore, "%")
+    #predicts = pca.score_samples(testData)
     
     model_Perceptron.fit(trainData, train['Survived'])
     trainScore = model_Perceptron.score(trainData, train['Survived'])*100
